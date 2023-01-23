1c62df4a42291eccbd06db8547c031ff15bea9ae
Matt Wilder <97206425+mattwilder@users.noreply.github.com>
2023-01-23 13:06:14 -0800

    kubernetes_node_taint: don't fail if the node has been deleted (#3)
    Return a warning instead of an error if the underlying node for an existing taint resource does not exist.
    
    Otherwise, `terraform plan` fails if there is a taint resource in your state file for a node that no longer exists.

--- kubernetes/resource_kubernetes_node_taint.go
+++ kubernetes/resource_kubernetes_node_taint.go
@@ -105,6 +105,14 @@ func resourceKubernetesNodeTaintRead(ctx context.Context, d *schema.ResourceData
 
 	node, err := nodeApi.Get(ctx, nodeName, metav1.GetOptions{})
 	if err != nil {
+		if statusErr, ok := err.(*errors.StatusError); ok && errors.IsNotFound(statusErr) {
+			// The node is gone so the resource should be deleted.
+			return diag.Diagnostics{{
+				Severity: diag.Warning,
+				Summary:  "Node has been deleted",
+				Detail:   fmt.Sprintf("The underlying node %q has been deleted. You should remove it from your configuration.", nodeName),
+			}}
+		}
 		return diag.FromErr(err)
 	}
 	nodeTaints := node.Spec.Taints
